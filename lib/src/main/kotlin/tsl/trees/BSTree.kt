f2baa248d6b89dfd07ab1cfa1cb19a47a236a863
Karim Shakirov <karimshak06@gmail.com>
2024-03-21 14:22:46 +0300

    feat: add delete method in BSTree

--- lib/src/main/kotlin/tsl/trees/BSTree.kt
+++ lib/src/main/kotlin/tsl/trees/BSTree.kt
@@ -12,7 +12,7 @@ class BSTree<K : Comparable<K>, V> : AbstractBinaryTree<K, V, BSTNode<K, V>>() {
 			return null
 		}
 
-		var currentNode: BSTNode<K, V>? = root
+		var currentNode: BSTNode<K, V> = root
 		while (true) {
 			if (nodeToInsert.key == currentNode.key) {
 				val oldValue = currentNode.value
@@ -36,3 +36,24 @@ class BSTree<K : Comparable<K>, V> : AbstractBinaryTree<K, V, BSTNode<K, V>>() {
 		}
 	}
 
+	override fun delete(key: K): V? {
+		val deletedValue? = search(key) ?: return null
+		deleteRecursively(root, key)
+		return deletedValue
+	}
+
+	private fun deleteRecursively(node: BSTNode<K, V>, key): BSTNode {
+		if (node == null) return null
+		if (key < node.key) node.leftChild = deleteRecursively(node.leftChild, key)
+		else if (key > node.key) node.rightChild = deleteRecursively(node.rightChild, key)
+		else if (key == node.key) {
+			if (node.leftChild == null || node.rightChild == null)
+				return node.leftChild ?: node.rightChild
+			else {
+				val minRightNode = getMinNode(node.rightChild)
+				node.key = minRightNode.key
+				node.value = minRightNode.value
+				node.rightChild = deleteRecursively(node.rightChild, minRightNode.key)
+			}
+		}
+	}
