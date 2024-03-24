997edc8005f54bac0edd4e1995a79259a7801c0e
Vlasenco Daniel <vlasenko.daniil26@gmail.com>
2024-03-24 20:54:17 +0300

    refactor: AVL tree class, explicitly mark public methods

--- lib/src/main/kotlin/tsl/trees/AVLTree.kt
+++ lib/src/main/kotlin/tsl/trees/AVLTree.kt
@@ -5,96 +5,60 @@ import tsl.nodes.AVLNode
 import kotlin.math.max
 
 class AVLTree<K : Comparable<K>, V> : AbstractBinaryTree<K, V, AVLNode<K, V>>() {
-    override fun insert(key: K, value: V): V? {
-        val replacedValue = search(key)        // null if key isn't in the tree
-        insertAndBalanceRecursively(root, key, value)
-        return replacedValue
+    public override fun insert(key: K, value: V): V? {
+        val oldValueByKey = search(key)        // null if key isn't in the tree
+
+        insertNodeAndBalanceRecursively(root, key, value)
+
+        return oldValueByKey
     }
 
-    private fun insertAndBalanceRecursively(node: AVLNode<K, V>?, key: K, value: V): AVLNode<K, V>? {
-        if (root == null) {
-            root = AVLNode(key, value)
-            return root
-        }
-        if (node == null) return AVLNode(key, value)
+    private fun insertNodeAndBalanceRecursively(
+        currNode: AVLNode<K, V>?,
+        keyToInsert: K,
+        valueToInsert: V
+    ): AVLNode<K, V> {
 
-        if (key < node.key) {
-            node.leftChild = insertAndBalanceRecursively(node.leftChild, key, value)
-        } else if (key > node.key) {
-            node.rightChild = insertAndBalanceRecursively(node.rightChild, key, value)
-        } else return node
-
-        updateHeight(node)
-
-        val balanceFactor = getBalanceFactor(node)
-
-        if (balanceFactor < -1) {                       // right cases
-            node.leftChild?.let {
-                if (key > it.key) {
-                    node.leftChild = rotateLeft(it)     // left-right case
-                }
-                return rotateRight(node)
-            }
-        } else if (balanceFactor > 1) {                 // left cases
-            node.rightChild?.let {
-                if (key < it.key) {
-                    node.rightChild = rotateRight(it)   // right-left case
-                }
-                return rotateLeft(node)
-            }
+        if (currNode == null) {
+            val newNode = AVLNode(keyToInsert, valueToInsert)
+            if (root == null) root = newNode
+            return newNode
         }
 
-        return node
-    }
+        when {
+            keyToInsert < currNode.key ->
+                currNode.leftChild = insertNodeAndBalanceRecursively(currNode.leftChild, keyToInsert, valueToInsert)
 
-    override fun delete(key: K): V? {
-        val deletedValue: V = search(key) ?: return null    // if key isn't in the tree, there's nothing to delete
-        deleteAndBalanceRecursively(root, key)
-        return deletedValue
-    }
+            keyToInsert > currNode.key ->
+                currNode.rightChild = insertNodeAndBalanceRecursively(currNode.rightChild, keyToInsert, valueToInsert)
 
-    private fun deleteAndBalanceRecursively(node: AVLNode<K, V>?, key: K): AVLNode<K, V>? {
-        if (node == null) return null           // node to be deleted was not found
+            else -> return currNode
+        }
 
-        if (key < node.key) {
-            node.leftChild = deleteAndBalanceRecursively(node.leftChild, key)
-        } else if (key > node.key) {
-            node.rightChild = deleteAndBalanceRecursively(node.rightChild, key)
-        } else if (node.leftChild == null || node.rightChild == null) {     // if node to be deleted has 0 or 1 child
-            return node.leftChild ?: node.rightChild
-        } else {                                                            // if node to be deleted has 2 children
-            val successor = getMinNode(node.rightChild)
-            if (successor != null) {
-                node.key = successor.key
-                node.value = successor.value
+        val balanceFactor = getBalanceFactor(currNode)
+        var balancedNode: AVLNode<K, V> = currNode
 
-                // delete successor node from tree
-                val temp = deleteAndBalanceRecursively(node.rightChild, successor.key)
-                if (temp != null) node.rightChild = temp
+        if (balanceFactor < -1) {                       // if left subtree got bigger
+            currNode.leftChild?.let {
+                if (keyToInsert > it.key) {                     // if inserted node has greater key
+                    currNode.leftChild = rotateLeft(it)         // perform left rotation first
+                }
+                balancedNode = rotateRight(currNode)        // anyway, perform right rotation
+            }
+        } else if (balanceFactor > 1) {                 // if right subtree got bigger
+            currNode.rightChild?.let {
+                if (keyToInsert < it.key) {                     // if inserted node has lesser key
+                    currNode.rightChild = rotateRight(it)       // perform right rotation first
+                }
+                balancedNode = rotateLeft(currNode)         // anyway, perform left rotation
             }
         }
 
-        updateHeight(node)
+        updateHeight(balancedNode.leftChild)
+        updateHeight(balancedNode.rightChild)
+        updateHeight(balancedNode)
 
-        val balanceFactor = getBalanceFactor(node)
-
-        if (balanceFactor < -1) {                       // right rotation cases
-            node.leftChild?.let {
-                if (getBalanceFactor(it) > 0) {         // left-right case
-                    node.leftChild = rotateLeft(it)
-                }
-                return rotateRight(node)
-            }
-        } else if (balanceFactor > 1) {                 // left rotation cases
-            node.rightChild?.let {
-                if (getBalanceFactor(it) < 0) {         // right-left case
-                    node.rightChild = rotateRight(it)
-                }
-                return rotateLeft(node)
-            }
-        }
-
-        return node
+        return balancedNode
     }
 
     private fun rotateRight(oldUpperNode: AVLNode<K, V>): AVLNode<K, V> {
@@ -103,9 +67,6 @@ class AVLTree<K : Comparable<K>, V> : AbstractBinaryTree<K, V, AVLNode<K, V>>() 
         newUpperNode.rightChild = oldUpperNode
 
         if (root == oldUpperNode) root = newUpperNode       // if root was rotated, set new root
-
-        updateHeight(oldUpperNode)
-        updateHeight(newUpperNode)
 
         return newUpperNode
     }
@@ -117,22 +78,81 @@ class AVLTree<K : Comparable<K>, V> : AbstractBinaryTree<K, V, AVLNode<K, V>>() 
 
         if (root == oldUpperNode) root = newUpperNode       // if root was rotated, set new root
 
-        updateHeight(oldUpperNode)
-        updateHeight(newUpperNode)
-
         return newUpperNode
     }
 
-    private fun updateHeight(node: AVLNode<K, V>): Int {
+    private fun updateHeight(node: AVLNode<K, V>?): Int {
+        if (node == null) return -1
         node.height = max(getHeight(node.leftChild), getHeight(node.rightChild)) + 1
         return node.height
     }
 
-    private fun getBalanceFactor(node: AVLNode<K, V>): Int {
-        return getHeight(node.rightChild) - getHeight(node.leftChild)
+    private fun getBalanceFactor(node: AVLNode<K, V>): Int =
+        getHeight(node.rightChild) - getHeight(node.leftChild)
+
+    private fun getHeight(node: AVLNode<K, V>?): Int = node?.height ?: -1
+
+    public override fun delete(key: K): V? {
+        val deletedValue: V = search(key) ?: return null    // if key isn't in the tree, there's nothing to delete
+
+        deleteAndBalanceRecursively(root, key)
+
+        return deletedValue
     }
 
-    private fun getHeight(node: AVLNode<K, V>?): Int {
-        return node?.height ?: -1
+    private fun deleteAndBalanceRecursively(
+        currNode: AVLNode<K, V>?,
+        keyToDelete: K
+    ): AVLNode<K, V>? {
+
+        when {
+            currNode == null -> return null         // node to be deleted was not found
+
+            keyToDelete < currNode.key ->
+                currNode.leftChild = deleteAndBalanceRecursively(currNode.leftChild, keyToDelete)
+
+            keyToDelete > currNode.key ->
+                currNode.rightChild = deleteAndBalanceRecursively(currNode.rightChild, keyToDelete)
+
+            (currNode.leftChild == null || currNode.rightChild == null) ->       // if node has 0 or 1 child
+                return currNode.leftChild ?: currNode.rightChild                 // replace it with its child
+
+            else -> {                                                           // if node has 2 children
+                val successor = getMinNodeRecursively(currNode.rightChild)
+                if (successor != null) {
+                    currNode.key = successor.key
+                    currNode.value = successor.value
+
+                    // delete successor node from tree
+                    val newSubtree = deleteAndBalanceRecursively(currNode.rightChild, successor.key)
+                    if (newSubtree != null) currNode.rightChild = newSubtree
+                }
+            }
+        }
+
+        val balanceFactor = getBalanceFactor(currNode)
+        var balancedNode: AVLNode<K, V> = currNode
+
+        if (balanceFactor < -1) {                       // if left subtree got bigger
+            currNode.leftChild?.let {
+                if (getBalanceFactor(it) > 0) {                 // if inserted node has greater key
+                    currNode.leftChild = rotateLeft(it)         // perform left rotation first
+                }
+                balancedNode = rotateRight(currNode)        // anyway, perform left rotation
+            }
+        } else if (balanceFactor > 1) {                 // if right subtree got bigger
+            currNode.rightChild?.let {
+                if (getBalanceFactor(it) < 0) {                 // if inserted node has greater key
+                    currNode.rightChild = rotateRight(it)       // perform right rotation first
+                }
+                balancedNode = rotateLeft(currNode)         // anyway, perform left rotation
+            }
+        }
+
+        updateHeight(balancedNode.leftChild)
+        updateHeight(balancedNode.rightChild)
+        updateHeight(balancedNode)
+
+        return balancedNode
     }
 }
