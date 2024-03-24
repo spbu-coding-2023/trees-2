ff71aecc3f943d03ba6d50fb1e4194b88a0cb13d
qruty <64466788+qrutyy@users.noreply.github.com>
2024-03-24 10:56:46 +0300

    feat: add 'fixAfterDeletion' method that help to rebalance the tree after delete

--- lib/src/main/kotlin/tsl/trees/RBTree.kt
+++ lib/src/main/kotlin/tsl/trees/RBTree.kt
@@ -145,4 +145,67 @@ class RBTree<K : Comparable<K>, V> : AbstractBinaryTree<K, V, RBNode<K, V>>() {
         }
         return newRoot
     }
+
+    private fun fixAfterDeletion(currentNode: RBNode<K, V>?): RBNode<K, V>? {
+        var fixedRoot = root
+        var node = currentNode
+
+        while (node != root && node?.color == RBNode.Color.Black) {
+            if (node == node.parent?.leftChild) {
+                var currentSibling: RBNode<K, V>? = node.parent?.rightChild
+                if (currentSibling?.color == RBNode.Color.Red) { // 1st case "currentSibling is red"
+                    currentSibling.color = RBNode.Color.Black
+                    node.parent?.color = RBNode.Color.Red
+                    fixedRoot = rotateLeft(node.parent, fixedRoot)
+                    currentSibling = node.parent?.rightChild  // transfer to other cases
+                }
+                else if (currentSibling?.leftChild?.color == RBNode.Color.Black && currentSibling.rightChild?.color == RBNode.Color.Black) {
+                    // 2nd case "currentSibling and its children are black"
+                    currentSibling.color = RBNode.Color.Red
+                    node = node.parent
+                } else {
+                    if (currentSibling?.rightChild?.color == RBNode.Color.Black) {
+                        // 3rd case "like 2nd but the left child is red"
+                        (currentSibling.leftChild )?.color = RBNode.Color.Black
+                        currentSibling.color = RBNode.Color.Red
+                        fixedRoot = rotateRight(currentSibling, fixedRoot)
+                        currentSibling = node.parent?.rightChild
+                    }
+                    currentSibling?.color = node.parent?.color!! // 4th case "black currentSibling, right child is black'
+                    node.parent?.color = RBNode.Color.Black
+                    currentSibling?.rightChild?.color = RBNode.Color.Black
+                    fixedRoot = rotateLeft(node.parent, fixedRoot)
+                    node = fixedRoot
+                }
+            } else {
+                var currentSibling: RBNode<K, V>? = node.parent?.leftChild
+                if (currentSibling?.color == RBNode.Color.Red) {
+                    // 1st case "red currentSibling"
+                    currentSibling.color = RBNode.Color.Black
+                    node.parent?.color = RBNode.Color.Red
+                    fixedRoot = rotateRight(node.parent, fixedRoot)
+                    currentSibling = node.parent?.leftChild  // transfer to other cases
+                }
+                // 2nd case "currentSibling and its children are black"
+                if (currentSibling?.leftChild?.color == RBNode.Color.Black && currentSibling.rightChild?.color == RBNode.Color.Black) {
+                    currentSibling.color = RBNode.Color.Red
+                    node = node.parent
+                } else {
+                    if (currentSibling?.leftChild?.color == RBNode.Color.Black) { // 3rd case "like 2nd but the right child is red"
+                        currentSibling.rightChild?.color = RBNode.Color.Black
+                        currentSibling.color = RBNode.Color.Red
+                        fixedRoot = rotateLeft(currentSibling, fixedRoot)
+                        currentSibling = node.parent?.leftChild
+                    }
+                    currentSibling?.color = node.parent?.color!! // 4th case "black currentSibling, left child is red'
+                    (node.parent)?.color = RBNode.Color.Black
+                    (currentSibling?.leftChild)?.color = RBNode.Color.Black
+                    fixedRoot = rotateRight(node.parent, fixedRoot)
+                    node = fixedRoot
+                }
+            }
+        }
+        node?.color = RBNode.Color.Black
+        return fixedRoot
+    }
 }
