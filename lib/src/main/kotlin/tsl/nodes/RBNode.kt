e8a07571adfe009ca8125c4d5100915205ed973c
qruty <64466788+qrutyy@users.noreply.github.com>
2024-03-24 12:51:20 +0300

    fix: move rotation methods to the tree file

--- lib/src/main/kotlin/tsl/nodes/RBNode.kt
+++ lib/src/main/kotlin/tsl/nodes/RBNode.kt
@@ -18,41 +18,5 @@ class RBNode<K : Comparable<K>, V>(key: K, value: V) : AbstractNode<K, V, RBNode
             node2.color = node1color
         }
     }
-
-    internal fun rotateLeft() {
-        val rightChild = this.rightChild ?: return
-        val dad = this.parent
-
-        this.switchColor(rightChild)
-        rightChild.leftChild?.parent = this
-        this.rightChild = rightChild.leftChild
-        rightChild.leftChild = this
-
-        when {
-            this == dad?.leftChild -> dad.leftChild = rightChild
-            this == dad?.rightChild -> dad.rightChild = rightChild
-        }
-
-        this.parent = rightChild
-        rightChild.parent = dad
-    }
-
-    internal fun rotateRight() {
-        val leftChild = this.leftChild ?: return
-        val dad = this.parent
-
-        this.switchColor(leftChild)
-        leftChild.rightChild?.parent = this
-        this.leftChild = leftChild.rightChild
-        leftChild.rightChild = this
-
-        when {
-            this == dad?.leftChild -> dad.leftChild = leftChild
-            this == dad?.rightChild -> dad.rightChild = leftChild
-        }
-
-        this.parent = leftChild
-        leftChild.parent = dad
-    }
 }
 
