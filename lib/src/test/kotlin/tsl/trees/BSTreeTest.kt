8ca04ce704a317a8b044ba04312b26a855f1f20c
Shakirov Karim <karimshak06@gmail.com>
2024-03-30 22:43:10 +0300

    test: add tests of search method of the BS tree

--- lib/src/test/kotlin/tsl/trees/BSTreeTest.kt
+++ lib/src/test/kotlin/tsl/trees/BSTreeTest.kt
@@ -95,6 +95,71 @@ class BSTreeTest {
     @Nested
     inner class SearchTests {
 
+        @Test
+        fun `search of non existing key should return null`() {
+            defaultFill(tree)
+
+            val expectedValue = null
+            val actualValue = tree.search(100)
+
+            assertEquals(expectedValue, actualValue)
+        }
+
+        @Test
+        fun `search of existing key that is in the root node should return the corresponding value`() {
+            defaultFill(tree)
+
+            val expectedValue = "java"
+            val actualValue = tree.search(30)
+
+            assertEquals(expectedValue, actualValue)
+        }
+
+        @Test
+        fun `search of existing key that is to the left of the root node should return the corresponding value`() {
+            defaultFill(tree)
+
+            val expectedValue = "kotlin"
+            val actualValue = tree.search(20)
+
+            assertEquals(expectedValue, actualValue)
+        }
+
+        @Test
+        fun `search of existing key that is to the right of the root node should return the corresponding value`() {
+            defaultFill(tree)
+
+            val expectedValue = "gnomik"
+            val actualValue = tree.search(40)
+
+            assertEquals(expectedValue, actualValue)
+        }
+
+        @Test
+        fun `search should not change an empty tree`() {
+            tree.search(42069)
+
+            val expectedStructure: List<Pair<Int, String>> = listOf()
+            val actualStructure: MutableList<Pair<Int, String>> = mutableListOf()
+            for ((key, value) in tree) actualStructure.add(Pair(key, value))
+
+            assertEquals(expectedStructure, actualStructure)
+        }
+
+        @Test
+        fun `search should not change a non-empty tree`() {
+            defaultFill(tree)
+
+            tree.search(40)
+
+            val expectedStructure = listOf(Pair(10, "kotik"), Pair(20, "kotlin"),
+                                           Pair(30, "java"), Pair(40, "gnomik"))
+
+            val actualStructure: MutableList<Pair<Int, String>> = mutableListOf()
+            for ((key, value) in tree) actualStructure.add(Pair(key, value))
+
+            assertEquals(expectedStructure, actualStructure)
+        }
     }
 
     @Nested
